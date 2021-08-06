c5f72d4964b2d3e8d0f3abc0f5a01417cc3df834
shlomo <shlomo.gevim@gmail.com>
2021-08-06 17:17:50 +0300

    its 8/6//2021 17:17

--- app/src/main/java/com/sg/generalpost10/Const.kt
+++ app/src/main/java/com/sg/generalpost10/Const.kt
@@ -3,4 +3,4 @@ package com.sg.generalpost10
 const val TOP="top"
 const val LEFT="left"
 const val RIGHT="right"
-const val BUTTOM="bottom"
+const val BOTTOM="bottom"
--- app/src/main/java/com/sg/generalpost10/MainActivity.kt
+++ app/src/main/java/com/sg/generalpost10/MainActivity.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var word1:TextView
+    lateinit var word2:TextView
     val ID_1 = 1
     val ID_2 = 2
 
@@ -24,22 +25,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun firstPost() {
         imageView.load(R.drawable.sad)
-       // word1=locateWord("word1,"העולם", TOP,50, LEFT,100)
-        word1=locateWord("word1","העולם", TOP,100, LEFT,200)
+
+       word1=locateWord(0,"בעולם אין בעיות בכלל", BOTTOM, RIGHT,0, 0,20,140)
+     // word2=locateWord(1,"חוץ מהבעיות שאתה יוצר", BOTTOM, RIGHT,0, 0,20,100)
+
+
+
+
+
     }
 
-    private fun locateWord(id:String, word: String, border1: String, margin1: Int, border2: String, margin2: Int): TextView {
+    private fun locateWord(index:Int, word: String, border1: String,border2: String, mLeft: Int,mUp: Int,mRight:Int,mBottom:Int): TextView {
       val textView=TextView(this)
 
         val lp = ConstraintLayout.LayoutParams(
             ConstraintLayout.LayoutParams.WRAP_CONTENT,
             ConstraintLayout.LayoutParams.WRAP_CONTENT
         )
+        val containSet = ConstraintSet()
+
         lp.apply {
-            if (border2=="left") marginStart=8.toPx()
-            if (border1=="top")topMargin=50.toPx()
-            //  marginEnd=20.toDp()
-            // bottomMargin=50.toDp()
+            if ((border1== LEFT)|| (border2== LEFT)) marginStart=mLeft.toPx()
+            if ((border1== TOP)|| (border2== TOP)) topMargin=mUp.toPx()
+            if ((border1== RIGHT)|| (border2== RIGHT)) marginEnd=mRight.toPx()
+            if ((border1== BOTTOM)|| (border2== BOTTOM)) bottomMargin=mBottom.toPx()
         }
 
         textView.layoutParams = lp
@@ -49,24 +58,37 @@ class MainActivity : AppCompatActivity() {
         mainLayout.addView(textView)
 
 
-
-
-
-
-        val containSet = ConstraintSet()
         containSet.clone(mainLayout)
 
-        containSet.connect(textView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-        containSet.connect(textView.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
+
+        if ((border1== LEFT)|| (border2== LEFT)){
+            containSet.connect(textView.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
+        }
+
+        if ((border1== TOP)|| (border2== TOP)){
+            containSet.connect(textView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+        }
+
+        if ((border1== RIGHT)|| (border2== RIGHT)){
+            containSet.connect(textView.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
+        }
+
+        if ((border1== BOTTOM)|| (border2== BOTTOM)){
+            containSet.connect(textView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
+        }
+
 
 /*
 
         containSet.connect(ID_2, ConstraintSet.TOP, ID_1, ConstraintSet.BOTTOM)
         containSet.connect(ID_2, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
         containSet.connect(ID_2, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)*/
+
+
+
+        //if (index==1) containSet.applyTo(mainLayout)
+
         containSet.applyTo(mainLayout)
-
-
 
 
 
